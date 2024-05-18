aba73776c58a675d133cf7556a73d84e90231822
tomascarreira <carreira_1012@hotmail.com>
2024-05-18 11:11:26 +0100

    Adiciona automatação na criação do executável

--- plush.py
+++ plush.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from parser import parse, Declaration, Type, TypeEnum
 from typeChecker import verify, Context as TypeContext
@@ -8,24 +9,28 @@ from pretty_print import pp_ast
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        print("Reading file: ", sys.argv[1])
         with open(sys.argv[1]) as file:
             input = file.read()
             ast = parse(input)
 
-        print("=====AST=====")
         verify(TypeContext(), ast)
-        pp_ast(ast)
+        # print("=====AST=====")
+        # pp_ast(ast)
 
-        print("=====Codegen=====")
         emitter = codegen(ast)
-        print("\n".join(emitter.lines))
-        print("\n".join(emitter.decls))
+        # print("=====Codegen=====")
+        # print("\n".join(emitter.lines))
+        # print("\n".join(emitter.decls))
 
-        outName = sys.argv[1].rsplit(".", 1)[0].rsplit("/", 1)[-1]+".ll"
+        outName = sys.argv[1].rsplit(".", 1)[0].rsplit("/", 1)[-1] + ".ll"
         with open(outName, "w") as out:
             out.write("\n".join(emitter.lines))
             out.write("\n".join(emitter.decls))
+
+        os.system(f"llc {outName} -o {outName.rsplit(".", 1)[0].rsplit("/", 1)[-1]+".s"}")  
+        os.system("make c_functions")  
+        os.system(f"gcc -c {outName.rsplit(".", 1)[0].rsplit("/", 1)[-1]+".s"} -o {outName.rsplit(".", 1)[0].rsplit("/", 1)[-1]+".o"}")
+        os.system(f"gcc c_functions.o {outName.rsplit(".", 1)[0].rsplit("/", 1)[-1]+".o"} -o {outName.rsplit(".", 1)[0].rsplit("/", 1)[-1]}")
 
     else:
         while True:
