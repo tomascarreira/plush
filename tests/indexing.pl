1ec5cd8c0839cbb4dc46fe70f4d90cb69389846a
tomascarreira <carreira_1012@hotmail.com>
2024-05-26 19:26:36 +0100

    Arranja bug num teste

--- tests/indexing.pl
+++ tests/indexing.pl
@@ -1,5 +1,5 @@
 function int_array(val size: int): [int];
-function print_int_array(val arr: [int]);
+function print_int_array(val arr: [int], val size: int);
 
 function main() {
   var arr: [int] := int_array(5);
@@ -9,5 +9,5 @@ function main() {
   arr[3] := 3;
   arr[4] := 4;
 
-  print_int_array(arr);
+  print_int_array(arr, 5);
 }
