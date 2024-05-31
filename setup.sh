e8ddc9623182e4f49a6dc8ce5951c6c9a04b0c5c
tomascarreira <carreira_1012@hotmail.com>
2024-05-31 21:09:11 +0100

    Muda o setup para especificar a versao do llvm e do gcc

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t plush .
-docker run -ti plush:latest nix-shell -p python311 python311Packages.ply llvm gcc
+docker run -ti plush:latest nix-shell -p python311 python311Packages.ply llvmPackages_16.libllvm gcc12
